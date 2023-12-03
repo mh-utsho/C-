a224b2a0065d00954debf452fb8ac2d5761f595b
mh-utsho <utshoutsho76@gmail.com>
2023-12-03 20:38:52 +0600

    findDuplicate

--- Node.cpp
+++ Node.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
-    
+
 }
+
+  int findDuplicate(std::vector<int>& v) {
+        std::unordered_set<int> m;
+        for (int x : v) {
+            if (m.find(x) != m.end()) {
+                return x;
+            }
+            m.insert(x);
+        }
+        return -1;  
+    }
+
+
+     
